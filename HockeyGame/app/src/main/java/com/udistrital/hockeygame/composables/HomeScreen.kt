6df208c0e30c461439e45895db71b68c507f1789
stephanny <stephannynayleth01@gmail.com>
2026-09-07 13:31:17 -0500

    New home screen ST style1

--- HockeyGame/app/src/main/java/com/udistrital/hockeygame/composables/HomeScreen.kt
+++ HockeyGame/app/src/main/java/com/udistrital/hockeygame/composables/HomeScreen.kt
@@ -1,24 +1,43 @@
 package com.udistrital.hockeygame.composables
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.udistrital.hockeygame.R
 import com.udistrital.hockeygame.enums.TypeScreen
 
 @Composable
-fun HomeScreen(onClickGame: () -> Unit){
 
-    Column() {
-        Text(stringResource(R.string.home_title))
+fun HomeScreen(onClickGame: () -> Unit) {
 
-        Text(stringResource(R.string.home_score))
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(R.string.home_title)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Text(
+            text = stringResource(R.string.home_score)
+        )
+        Spacer(modifier = Modifier.height(32.dp))
 
         Button(onClick = onClickGame) {
             Text(stringResource(R.string.home_btn_play))
         }
     }
-
 }
